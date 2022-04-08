66d3ccb6c62c94456907c5f393442db4e877fe67
sachithrangana96 <sachithrangana96@gmail.com>
2022-04-08 10:26:56 +0530

    Files created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-
 import './App.css';
+import { Routes,BrowserRouter, Route } from "react-router-dom";
+import Home from './pages/Home';
+import AddEditBlog from './pages/AddEditBlog';
+import Blog from './pages/Blog';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Blog</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/addBlog' element={<AddEditBlog />} />
+        <Route path='/editBlog/:id' element={<AddEditBlog />} />
+        <Route path='/blog/:id' element={<Blog />} />
+        <Route path='/about' element={<About />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
 
